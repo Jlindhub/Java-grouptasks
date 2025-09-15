1a7d29b73e5cc3bb60f9a2541e5d88effc3c9501
aletheos <aletheos@outlookcom>
2025-09-15 14:11:16 +0200

    Add a getter for the player character to Main
    Make cosmetic change to comment

--- Uppgifter/adventure_game/Main.java
+++ Uppgifter/adventure_game/Main.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
  * <h6>Puzzle game, inspired by Colossal Cave Adventure and Myst</h6>
  * <p>
  * The game consists of<br>
- * - {@link GameCharacter characters}<br>
- * - {@link GameRoom rooms}<br>
- * - {@link GameItem items}<br>
+ * • {@link GameCharacter characters}<br>
+ * • {@link GameRoom rooms}<br>
+ * • {@link GameItem items}<br>
  * The player character is the Id of the player by which they move through and interact with the world. A room has a
  * number of exits accessed by directional keywords, and items are things in the world that can be used by or carried
  * by characters.
@@ -17,7 +17,18 @@ public class Main {
 	private ArrayList<GameRoom> rooms;
 	private ArrayList<GameItem> items;
 
-	public static void main(String[] args) {
+    // The Id is always the first
+    private GameCharacter player() {
+        return characters.getFirst();
+    }
 
+    public Main(ArrayList<GameCharacter> characters, ArrayList<GameRoom> rooms, ArrayList<GameItem> items) {
+        this.characters = characters;
+        this.rooms = rooms;
+        this.items = items;
+    }
+
+    public static void main(String[] args) {
+        //Main game = new Main();
 	}
 }
