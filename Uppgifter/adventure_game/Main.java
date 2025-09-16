a3340dabfc5a74a7fc6178a69f2c5fe285cf0d18
aletheos <aletheos@outlookcom>
2025-09-16 10:15:22 +0200

    Have a mild existential crisis

--- Uppgifter/adventure_game/Main.java
+++ Uppgifter/adventure_game/Main.java
@@ -32,6 +32,16 @@ public class Main {
 		}
 
     public static void main(String[] args) {
+			var description = """
+			A large circular room with non-descript wooden doors in each
+			of the cardinal directions (east, west, north, and south).
+			A stone plinth stands in the middle of the room.
+			""";
+			//var roomExit = new GameDoor();
+			/* Hmm, now I have to refer to another already made room... before I can make the exit
+			 * to this room...??? These parts are all dependent on each other!
+			*/
+			//GameRoom startingArea = new GameRoom();
 			//Main game = new Main();
 			//game.run();
 	}
